e5520c9521462271841cbd99e1a110edae11f5f6
Igor Lemos Vicente <igorlemosvicente@gmail.com>
2017-10-01 16:03:58 -0300

    Cria um processo qualquer.

--- Makefile
+++ Makefile
@@ -174,6 +174,7 @@ UPROGS=\
 	_usertests\
 	_wc\
 	_zombie\
+	_proc_teste
 
 fs.img: mkfs README $(UPROGS)
 	./mkfs fs.img README $(UPROGS)
@@ -243,6 +244,7 @@ qemu-nox-gdb: fs.img xv6.img .gdbinit
 EXTRA=\
 	mkfs.c ulib.c user.h cat.c echo.c forktest.c grep.c kill.c\
 	ln.c ls.c mkdir.c rm.c stressfs.c usertests.c wc.c zombie.c\
+	proc_teste.c\
 	printf.c umalloc.c\
 	README dot-bochsrc *.pl toc.* runoff runoff1 runoff.list\
 	.gdbinit.tmpl gdbutil\
---
+++ proc_teste.c
@@ -0,0 +1,23 @@
+#include "types.h"
+#include "stat.h"
+#include "user.h"
+
+#define OUTPUT_STREAM 1
+
+int main(void) {
+  int pid;
+  pid = fork();
+  if (pid == 0) {
+    int i;
+    printf(OUTPUT_STREAM, "Filho: %d\n", getpid());
+    while(i++ < 1000000000);
+  } else if (pid > 0) {
+    printf(OUTPUT_STREAM, "Pai: %d\n", getpid());
+    printf(OUTPUT_STREAM, "Esperando filho terminar.\n");
+    wait();
+    printf(OUTPUT_STREAM, "Filho terminou.\n");
+  } else {
+    printf(OUTPUT_STREAM, "Erro\n");
+  }
+  exit();
+}
